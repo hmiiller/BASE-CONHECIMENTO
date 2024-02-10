65b2d4df9ff155d3bb5788c30f0355f0bf797da5
Humberto Müller <hfagundesp@gmail.com>
2024-02-10 13:29:44 -0300

    up
    up

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return 'Usuário cadastrado com sucesso!'
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
